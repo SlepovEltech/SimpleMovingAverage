40b3b58c759587edbb55bd01420470f5b2c8f282
AES <slepoveltech>
2023-09-20 10:16:13 +0300

    time duration done

--- SimpleMovingAverage.h
+++ SimpleMovingAverage.h
@@ -6,32 +6,57 @@
 #define SMA_SIMPLEMOVINGAVERAGE_H
 
 #include <deque>
-
-
-//class SimpleMovingAverage {
-//public:
-//    SimpleMovingAverage(int windowSize);
-//    double getFilteredValue(double newValue);
-//    ~SimpleMovingAverage();
-//private:
-//    int windowSize;
-//    double prevSMA;
-//    std::deque<double> buffer;
-//
-//};
+#include <numeric>
 
 template <typename T>
 class SimpleMovingAverage {
-public:
-    SimpleMovingAverage(int windowSize);
-    T getFilteredValue(T newValue);
-    ~SimpleMovingAverage();
 private:
     int windowSize;
     T prevSMA;
     std::deque<T> buffer;
+public:
+    SimpleMovingAverage(int windowSize_){
+        windowSize = windowSize_;
+        buffer = std::deque<T>(windowSize_, 0);
+        prevSMA = 0;
+    }
+    T getFilteredValue(T newValue){
+        T elements_sum = 0;
+        T newSMA = 0;
+        T frontValue = this->buffer.front();
+        this->buffer.pop_front();
+        this->buffer.push_back(newValue);
 
+        if(this->buffer.size() < this->windowSize){
+            elements_sum = std::accumulate(this->buffer.begin(), this->buffer.end(), 0);
+            newSMA = elements_sum / this->windowSize;
+        }
+        else{
+            newSMA = this->prevSMA + (newValue - frontValue)/this->windowSize;
+            this->prevSMA = newSMA;
+        }
+        this->prevSMA = newSMA;
+        return newSMA;
+    }
+    ~SimpleMovingAverage(){
+
+    }
+
+    T getFilteredValueSum(T newValue){
+        T elements_sum = 0;
+        T newSMA = 0;
+
+        T frontValue = this->buffer.front();
+        this->buffer.pop_front();
+        this->buffer.push_back(newValue);
+
+        for(auto element : this->buffer){
+            elements_sum += element;
+        }
+        newSMA = elements_sum / this->windowSize;
+        this->prevSMA = newSMA;
+
+        return newSMA;
+    }
 };
-
-
 #endif //SMA_SIMPLEMOVINGAVERAGE_H
--- main.cpp
+++ main.cpp
@@ -1,54 +1,29 @@
 #include <iostream>
 #include <ctime>
-#include <deque>
-#include <numeric>
-//#include "SimpleMovingAverage.h"
+#include <chrono>
 
-#define TICKS_NUM 20
+#include "SimpleMovingAverage.h"
 
-template <typename T>
-class SimpleMovingAverage {
-private:
-    int windowSize;
-    T prevSMA;
-    std::deque<T> buffer;
-public:
-    SimpleMovingAverage(int windowSize_){
-        windowSize = windowSize_;
-        buffer = std::deque<T>(windowSize_, 0);
-        prevSMA = 0;
-    }
-    T getFilteredValue(T newValue){
-        T elements_sum = 0;
-        T newSMA = 0;
-        if(this->buffer.size() <= this->windowSize){
-            this->buffer.push_back(newValue);
-            elements_sum = std::accumulate(this->buffer.begin(), this->buffer.end(), 0);
-            newSMA = elements_sum / this->windowSize;
-            this->prevSMA = newSMA;
-        }
-        else{
-            T frontValue = this->buffer.front();
-            newSMA = this->prevSMA + (newValue - frontValue)/this->windowSize;
-            this->buffer.pop_front();
-            this->buffer.push_back(newValue);
-            this->prevSMA = newSMA;
-        }
-        return newSMA;
-    }
-    ~SimpleMovingAverage(){
+#define TICKS_NUM 1000000
 
-    }
-};
-
+using namespace std;
+using namespace chrono;
 int main() {
-    std::srand(std::time(nullptr));
+    srand(std::time(nullptr));
     SimpleMovingAverage<float> filter(8);
+
+    // Get starting timepoint
+    auto start = high_resolution_clock::now();
 
     for (int i = 0; i < TICKS_NUM; i++) {
         double value = 1.0 + (std::rand() / (static_cast<double>(RAND_MAX) + 1.0)) * 100;
-        std::cout << value << " | filtered: " << filter.getFilteredValue(value) << std::endl;
-
+        cout << "Random: " << value << " | filtered: " << filter.getFilteredValueSum(value) << endl;
     }
+
+    auto stop = high_resolution_clock::now();
+    auto duration = duration_cast<microseconds>(stop - start);
+    double performance = static_cast<double>(TICKS_NUM)/duration.count();
+    cout  << "Time taken by function: " << duration.count() << " usec" << endl;
+    cout  << "Performance: " << performance << " ticks/usec" << endl;
     return 0;
 }
